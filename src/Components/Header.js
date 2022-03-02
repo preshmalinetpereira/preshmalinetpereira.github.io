94eca0596866a841719818d9dc30be0a89d3e301
preshmalinetpereira <preshmalinetpereira@gmail.com>
2022-03-02 09:48:33 -0500

    Added blog to header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,14 +9,15 @@ class Header extends Component {
 
         <nav id="nav-wrap">
 
-          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-          <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+          {/* <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+          <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a> */}
 
           <ul id="nav" className="nav">
             <li><NavLink exact="true" className={"homeNav"} to="/">Home</NavLink></li>
             <li><NavLink exact="true" className={"aboutNav"} to="/about">About</NavLink></li>
             {/* <li><NavLink exact="true" className={"resumeNav"} to="/resume">Resume</NavLink></li> */}
             <li><NavLink exact="true" className={"portfolioNav"} to="/portfolio">Portfolio</NavLink></li>
+            <li><NavLink exact="true" className={"blogNav"} to="#https://preshmalinetpereira.wordpress.com">Blog</NavLink></li>
             {/* <li><NavLink exact="true" className={"contactNav"} to="/contact">Contact</NavLink></li> */}
           </ul>
         </nav>
