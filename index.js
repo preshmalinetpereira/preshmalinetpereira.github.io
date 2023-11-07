ea973915aeaf5442220d17786c843d062a59628b
preshmalinetpereira <preshmalinetpereira@gmail.com>
2023-11-06 21:59:54 -0500

    updated website

--- index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './src/App';
-import registerServiceWorker from './src/registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
