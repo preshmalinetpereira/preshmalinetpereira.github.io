dba2d20c17d0eb2af420f120090c13f8206c6e92
preshmalinetpereira <preshmalinetpereira@gmail.com>
2024-06-03 09:31:06 -0400

    test 4

--- next.config.js
+++ next.config.js
@@ -2,22 +2,21 @@
 const nextConfig = {
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+      },
+      { hostname: "icons.duckduckgo.com" },
+      { hostname: "res.cloudinary.com" },
+      { hostname: "www.google.com" },
+      { hostname: "images.unsplash.com" },
+      { hostname: "drive.google.com"}
+    ],
+  },
 };
 
-module.exports = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "cdn.sanity.io",
-          port: "",
-        },
-        { hostname: "icons.duckduckgo.com" },
-        { hostname: "res.cloudinary.com" },
-        { hostname: "www.google.com" },
-        { hostname: "images.unsplash.com" },
-        { hostname: "drive.google.com"}
-      ],
-    },
-  };
+module.exports = nextConfig
 
